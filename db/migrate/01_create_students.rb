8390c04dae4c59d91100f30893d00a40e53fd320
lhsharing <daley.luis@gmail.com>
2019-03-25 13:19:30 -0400

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,18 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+
+    def change
+        create_table :students do |t|
+            t.string :name
+            #t.string :grade
+            #t.integer :age
+            #t.string :hometown
+        end
+    end
+
+    def up
+    end
+
+    def down
+    end
+
 end
